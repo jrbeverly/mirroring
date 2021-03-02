f2721a93259c1370a6054b8a056f1535dc74beea
jrbeverly <jonathan@jrbeverly.me>
2021-03-01 19:50:21 -0600

    Templates setting

--- tools/emit.bash
+++ tools/emit.bash
@@ -1,7 +1,27 @@
 #!/bin/bash
-DIR="$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) )"
+set -euo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR_ROOT="$( dirname "${DIR}" )"
 
-## Need a list of them, and templating
-
-# Mass import all of the projects
 #terraform import gitlab_project.example richardc/example
+
+REPOSITORIES="$@"
+TEMPLATE_FILE="${DIR}/templates/repository.tf"
+TEMP_DIR="${DIR}/tmp"
+
+counter=0
+rm -rf "${TEMP_DIR}"
+mkdir -p "${TEMP_DIR}"
+while IFS= read -r repo; do
+    echo "The repository: ${counter} | $repo"
+    counter=$((counter+1))
+
+    nameRepo=`echo $repo | tr / _`
+    git clone git@github.com:${repo}.git "${TEMP_DIR}/${counter}"
+    
+    export GIT_NAME=`cat "${TEMP_DIR}/${counter}/.repository/index.json" | jq -r '.slug'`
+    export GIT_TOPICS=`cat "${TEMP_DIR}/${counter}/.repository/index.json" | jq -r '.topics'`
+
+    mkdir -p "${DIR_ROOT}/repository/${counter}"
+    envsubst < "${TEMPLATE_FILE}" > "${DIR_ROOT}/repository/${counter}/main.tf"
+done < "${DIR}/data/${REPOSITORIES}"
