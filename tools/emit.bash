81f09ec6021f193504e6a26a3404156e303238ba
jrbeverly <jonathan@jrbeverly.me>
2021-03-01 18:05:23 -0600

    config to

---
+++ tools/emit.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR="$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) )"
+
+## Need a list of them, and templating
+
+# Mass import all of the projects
+#terraform import gitlab_project.example richardc/example
